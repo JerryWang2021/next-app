579a59474461a59a6e116f5ac7157d3baf20e2bc
Zhi Wang <jerrywang65@hotmail.com>
2023-09-20 19:16:37 -0700

    Handling errors

---
+++ app/NavBar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="flex bg-slate-200 p-5">
+      <Link href="/" className="mr-5">
+        Next.js
+      </Link>
+      <Link href="/users">User</Link>
+    </div>
+  );
+};
+
+export default NavBar;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./new/UserTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: { sortOrder: string };
@@ -9,6 +10,10 @@ const UserPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
+      <Suspense fallback={<p>Loading...</p>}></Suspense>
       <UserTable sortOrder={sortOrder} />
     </>
   );
